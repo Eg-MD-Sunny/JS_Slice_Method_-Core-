31121f926f170475b7bd8abd5dc1b58fa325fbca
Md Naimur Zaman Sunny <egmdsunny@gmail.com>
2022-09-12 12:48:22 +0600

    Adding New Method

--- app.js
+++ app.js
@@ -2,3 +2,27 @@
 let str = 'Cyber Firebug'
 console.log(str.slice(0,5))
 console.log(str.slice(0,-5))
+
+
+//====>Example 02
+let students = ['A','B','C','D','E']
+let result = students.slice(1,4);
+console.log(result)
+
+
+//====>Example 03
+let latters = ['A','B','C','D','E']
+let new_array = latters.slice(-4,-3);
+console.log(new_array)
+
+
+//====>Example 04
+let arr = ['A','B','C','D','E']
+let new_arr = latters.slice(0,9);
+console.log(new_arr)
+
+
+//====>Example 05
+let studentInfo = ['Naim','Borno','Farhan','Sad','Omar']
+let newValue = studentInfo.slice(undefined,4)
+console.log(newValue);
